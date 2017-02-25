d0e442e19b8365bad74bbe2f1acc2240e8141c8c
Piotr Sobczyński <ps.xaos@gmail.com>
2017-02-25 14:39:08 +0100

    commit 2

--- app/src/main/java/pl/xaos/learngwithbook/MainActivity.java
+++ app/src/main/java/pl/xaos/learngwithbook/MainActivity.java
@@ -1,13 +1,73 @@
 package pl.xaos.learngwithbook;
 
+import android.app.ListActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+//import android.R;
+//change 1
+//commit 2
+public class MainActivity extends ListActivity {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String[] items = {"acece", "bcdc", "bd", "ccdcwdc", "dcrcee", "ecerccc", "feee",
+            "acece", "bcdc", "bd", "ccdcwdc", "dcrcee", "ecerccc", "feee"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setListAdapter(new IconicAdapter());
+        //setContentView(R.layout.activity_main);
+
+
     }
+
+    class IconicAdapter extends ArrayAdapter<String> {
+        IconicAdapter() {
+            super(MainActivity.this, R.layout.row, R.id.label, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View row = super.getView(position, convertView, parent);
+            ViewHolder holder = (ViewHolder) row.getTag();
+
+            if (holder == null) {
+                holder = new ViewHolder(row);
+                row.setTag(holder);
+            }
+
+
+            if (items[position].length() < 5) {
+                holder.icon.setImageResource(android.R.drawable.star_on);
+
+            } else {
+
+                holder.icon.setImageResource(android.R.drawable.star_off);
+
+            }
+
+            TextView size = holder.size;
+            size.setText(items[position].length()+"");
+            return row;
+        }
+    }
+
+    class ViewHolder {
+        ImageView icon = null;
+        TextView size = null;
+
+        ViewHolder(View row) {
+            this.icon = (ImageView) row.findViewById(R.id.image);
+            this.size = (TextView) row.findViewById(R.id.size);
+        }
+
+    }
+
 }
